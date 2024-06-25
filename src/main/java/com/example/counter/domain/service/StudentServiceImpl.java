9a7ca0304aca4ad46eada705fb6527e6ac63ba2c
seo1016 <seoseunghun3@gmail.com>
2024-06-25 20:12:15 +0900

    Refactor :: 불필요한 import 제거

--- src/main/java/com/example/counter/domain/service/StudentServiceImpl.java
+++ src/main/java/com/example/counter/domain/service/StudentServiceImpl.java
@@ -6,8 +6,6 @@ import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.apache.coyote.BadRequestException;
 import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
